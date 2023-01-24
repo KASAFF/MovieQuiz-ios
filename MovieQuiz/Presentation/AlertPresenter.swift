84efa3a6912067d1ff6eb4faa13b3d024b268dbe
KASAFF <91468100+KASAFF@users.noreply.github.com>
2023-01-24 21:37:45 +0300

    add: tests

--- MovieQuiz/Presentation/AlertPresenter.swift
+++ MovieQuiz/Presentation/AlertPresenter.swift
@@ -20,6 +20,7 @@ protocol AlertPresenterProtocol {
         let alert = UIAlertController(title: model.title, // заголовок всплывающего окна
                                       message: model.message, // текст во всплывающем окне
                                       preferredStyle: .alert)
+        alert.view.accessibilityIdentifier = "Game results"
 
         let action = UIAlertAction(title: model.buttonText, style: .default) { _ in
             model.completion()
